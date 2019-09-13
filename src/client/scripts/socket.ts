1f66a7616e60ad0e4ee8cdb7105c0a180add387b
Abbas Battalov <Abbas_Battalov@epam.com>
2019-09-13 18:09:02 +0400

    rework map

--- src/client/scripts/socket.ts
+++ src/client/scripts/socket.ts
@@ -3,7 +3,8 @@ import { Socket } from 'socket.io';
 import { Movement } from '../../common/interfaces';
 
 const io = require('socket.io-client');
-const socket = io(process.env.NODE_ENV === 'production' ? 'https://serene-dawn-02472.herokuapp.com/' : 'http://localhost:5000');
+// const socket = io(process.env.NODE_ENV === 'production' ? 'https://serene-dawn-02472.herokuapp.com/' : 'http://localhost:5000');
+const socket = io('https://serene-dawn-02472.herokuapp.com/');
 
 export function registration(name: string): Observable<Socket> {
   const subject = new Subject<Socket>();
