5d400e66bb358711b09f12d4d94eb14c0c9511bf
Abbas Battalov <Abbas_Battalov@epam.com>
2019-09-27 19:03:17 +0400

    added renderer konva and physics engine p2js

--- src/client/scripts/socket.ts
+++ src/client/scripts/socket.ts
@@ -4,8 +4,8 @@ import { Movement } from '../../common/interfaces';
 
 const io = require('socket.io-client');
 // const socket = io(process.env.NODE_ENV === 'production' ? 'https://serene-dawn-02472.herokuapp.com/' : 'http://localhost:5000');
-const socket = io('http://localhost:5000');
-// const socket = io('https://serene-dawn-02472.herokuapp.com/');
+// const socket = io('http://localhost:5000');
+const socket = io('https://serene-dawn-02472.herokuapp.com/');
 
 export function registration(name: string): Observable<Socket> {
   const subject = new Subject<Socket>();
